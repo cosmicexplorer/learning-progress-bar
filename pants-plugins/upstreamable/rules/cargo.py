ac2317dc746250e541eb1e93e15e20140d726b20
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-12-16 00:46:01 -0800

    ./pants --print-exception-stacktrace --no-process-execution-{cleanup-local-dirs,use-local-cache} --v2 --no-v1 -ldebug binary inference creates a pex with a rust binary inside!!!

--- pants-plugins/upstreamable/rules/cargo.py
+++ pants-plugins/upstreamable/rules/cargo.py
@@ -1,9 +1,11 @@
 import dataclasses
+import logging
 import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
+from pants.backend.python.rules.pex_from_target_closure import PythonResourceTarget, PythonResources
 from pants.build_graph.address import Address
 from pants.engine.console import Console
 from pants.engine.fs import Digest, DirectoriesToMerge, Snapshot
@@ -22,6 +24,9 @@ from pants.subsystem.subsystem import Subsystem
 from pants.util.collections import Enum
 
 from upstreamable.targets.cargo_subproject import CargoSubproject
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -74,13 +79,18 @@ class Cargo:
   def release_mode_subdir(self) -> str:
     return 'release' if self.release_mode else 'debug'
 
+  def get_expected_output_binary_file(self, cargo_target: CargoTargetAdaptor) -> str:
+    return os.path.join('target',
+                        self.release_mode_subdir,
+                        str(cargo_target.binary_name))
+
   def create_execute_process_request(
       self,
       cargo_target: CargoTargetAdaptor,
       source_root_stripped_sources: SourceRootStrippedSources,
       command: CargoCommands,
   ) -> ExecuteProcessRequest:
-    return ExecuteProcessRequest(
+    ret = ExecuteProcessRequest(
       argv=command.create_cargo_command_argv(self.launcher_path),
       input_files=source_root_stripped_sources.snapshot.directory_digest,
       description=f'Execute cargo to build the request {cargo_target}!',
@@ -90,8 +100,10 @@ class Cargo:
         'PATH': os.environ['PATH'],
         'MODE': self.release_mode_subdir,
       },
-      output_files=(str(cargo_target.binary_name),),
+      output_files=(self.get_expected_output_binary_file(cargo_target),),
     )
+    logger.debug(f'creating process execution request for cargo: {ret}')
+    return ret
 
 
 @rule
@@ -116,21 +128,19 @@ def filter_cargo_buildable_targets(hts: HydratedTargets, cargo: Cargo) -> ManyCa
 @dataclass(frozen=True)
 class CargoBuildResult:
   snapshot: Snapshot
-  binary_path: RelPath
-
-  @property
-  def binary_digest(self) -> Digest:
-    return self.snapshot.directory_digest
 
   def __post_init__(self):
     """Assert that the snapshot contains exactly the single output file requested."""
-    assert len(self.snapshot.files) == 1
-    assert str(self.binary_path.path) == self.snapshot.files[0]
+    try:
+      assert len(self.snapshot.files) == 1
+    except AssertionError as e:
+      raise TypeError(f'failed to validate {self.__class__}: {e}. self was: {self}') from e
 
 
 @rule
 async def execute_cargo(buildable_target: CargoTargetAdaptor, cargo: Cargo) -> CargoBuildResult:
-  stripped_sources = await Get[SourceRootStrippedSources](HydratedTarget(testable_target.address, testable_target))
+  stripped_sources = await Get[SourceRootStrippedSources](
+    HydratedTarget(buildable_target.address, buildable_target, ()))
   exe_res = await Get[ExecuteProcessResult](ExecuteProcessRequest,
                                             cargo.create_execute_process_request(
                                               cargo_target=buildable_target,
@@ -138,12 +148,19 @@ async def execute_cargo(buildable_target: CargoTargetAdaptor, cargo: Cargo) -> C
                                               command=CargoCommands.build,
                                             ))
   snapshot = await Get[Snapshot](Digest, exe_res.output_directory_digest)
-  return CargoBuildResult(snapshot=snapshot, binary_path=RelPath(buildable_target.binary_name))
+  return CargoBuildResult(snapshot=snapshot)
+
+
+@rule
+async def collect_built_cargo_resources(buildable_target: CargoTargetAdaptor) -> PythonResources:
+  res = await Get[CargoBuildResult](CargoTargetAdaptor, buildable_target)
+  return PythonResources(res.snapshot)
 
 
 @rule
 async def execute_cargo_test(testable_target: CargoTargetAdaptor, cargo: Cargo) -> TestResult:
-  stripped_sources = await Get[SourceRootStrippedSources](HydratedTarget(testable_target.address, testable_target, ()))
+  stripped_sources = await Get[SourceRootStrippedSources](
+    HydratedTarget(testable_target.address, testable_target, ()))
   exe_res = await Get[FallibleExecuteProcessResult](ExecuteProcessRequest,
                                                     cargo.create_execute_process_request(
                                                       cargo_target=testable_target,
@@ -172,9 +189,11 @@ def rules():
     optionable_rule(Cargo.Factory),
     make_cargo,
     RootRule(CargoTargetAdaptor),
+    UnionRule(PythonResourceTarget, CargoTargetAdaptor),
     UnionRule(TestTarget, CargoTargetAdaptor),
     filter_cargo_buildable_targets,
     execute_cargo,
+    collect_built_cargo_resources,
     execute_cargo_test,
     # build_rust,
   ]
