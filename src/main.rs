26c9bd11efc8375a643608a81a23358561840163
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-06-28 02:08:06 -0400

    add a new cargo project

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+/*
+ * Description: A progress bar that uses statistics.
+ *
+ * Copyright (C) 2022 Danny McClanahan <dmcC2@hypnicjerk.ai>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+//! A progress bar that uses statistics.
+
+#![deny(rustdoc::missing_crate_level_docs)]
+/* Make all doctests fail if they produce any warnings. */
+#![doc(test(attr(deny(warnings))))]
+#![deny(clippy::all)]
+
+fn main() {
+  println!("hello, world!");
+}
